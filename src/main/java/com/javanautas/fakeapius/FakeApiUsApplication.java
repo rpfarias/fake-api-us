a7f3b365558d99d64cbddf4f98fc65ebbcb41c04
rafael <rpf1rafael@gmail.com>
2024-05-04 21:16:43 -0300

    Ajustes

--- src/main/java/com/javanautas/fakeapius/FakeApiUsApplication.java
+++ src/main/java/com/javanautas/fakeapius/FakeApiUsApplication.java
@@ -2,6 +2,8 @@ package com.javanautas.fakeapius;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
